d2250ec5548fea0ebefd39f4132217cf85ad027f
Adem <112782893+adeemmm23@users.noreply.github.com>
2024-05-06 13:06:54 +0100

    Refactor PageCubit to use double instead of int for better accuracy

--- lib/pages/views/passwords_generate/bloc/page_bloc.dart
+++ lib/pages/views/passwords_generate/bloc/page_bloc.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class PageCubit extends Cubit<int?> {
-  PageCubit() : super(null);
+class PageCubit extends Cubit<double> {
+  PageCubit() : super(0);
 
   void nextPage() => emit(1);
 }
